ffed9ad05baf866ca2ec91bbaff366f03bb4ea61
Rafael Fischer <rafaelfischer@gmail.com>
2025-09-16 16:22:10 -0300

    Criação da estrutura das pastas e alteração do arquivo base de exemplo

---
+++ exercicios/ex_base.py
@@ -0,0 +1,19 @@
+"""
+  Objetivo do programa: 
+  Data da criacao: YYYY-MM-DD
+  Criado por: @programacaomentoria
+""" 
+
+# lugar no programa onde declaro variaveis
+# nome_variavel: tipo_variavel
+# exemplo: nome: str
+# exemplo: idade: int
+
+# nome_constante = valor_constante
+# exemplo: PI = 3.14
+
+
+# lugar no programa onde escrevo as instrucoes
+# exemplo: print('Digite seu nome:')
+# exemplo: nome = input()
+# exemplo: print('Olá ', nome)
